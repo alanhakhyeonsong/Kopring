c2a51d0b936328b8b9d4d7300b8690aca24ba717
alanhakhyeonsong <songs4805@naver.com>
2023-03-31 16:38:38 +0900

    Feat: DTO 생성 (java)

---
+++ src/main/kotlin/me/ramos/kopring/dto/book/request/BookRequest.java
@@ -0,0 +1,15 @@
+package me.ramos.kopring.dto.book.request;
+
+public class BookRequest {
+
+  private String name;
+
+  public BookRequest(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+}
